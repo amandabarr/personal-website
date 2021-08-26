a564b2c688e928a70b47b484c070a0020a4e36e8
Amanda Barr <amanda.j.webster86@gmail.com>
2021-08-26 16:08:42 -0700

    Added route to about me page

--- server.py
+++ server.py
@@ -11,5 +11,16 @@ def hello():
     return render_template("homepage.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
